dce44968bfd065d4c72d29624636471b62bb4787
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-21 22:07:29 +0200

    Adding a header with basic css styling in the header component.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { useEffect } from 'react'
-import 
 
 
 const Header = () => {
-  useEffect(() => {
-    async await get
-    return () => {
-      cleanup
-    }
-  }, [input])
   return (
-    <>
-      
-    </>
+    <div className="header">
+      hej
+    </div>
   )
 }
 
