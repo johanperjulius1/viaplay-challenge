1b65a35ac81046b8e4a18c694c77be8fd3ceb6ee
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-21 21:12:39 +0200

    Adding first test. Starting on header component.

---
+++ cypress/integration/visitorCanSeeSeries.feature.js
@@ -0,0 +1,17 @@
+describe("Visitor can see cards with series", () => {
+  beforeEach("xxx", () => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga",
+      response: "fixture:series.json",
+    });
+    cy.visit("http://localhost:3000");
+  });
+
+  it("user can see basic interface", () => {
+    cy.get('[data-cy="header"]').should("be.visible");
+    cy.get['.display-show'].to.have.lengthOf(10)
+  });
+});
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useEffect } from 'react'
+import 
+
+
+const Header = () => {
+  useEffect(() => {
+    async await get
+    return () => {
+      cleanup
+    }
+  }, [input])
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Header
