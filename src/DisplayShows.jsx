90e1dfaf3c63fd7c8d6c8791195da200c2e6d5dd
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-28 00:18:55 +0100

    Adding axios, completing DisplayShows component which fetches data from the api and displays the shows, modifying styling.

--- src/DisplayShows.jsx
+++ src/DisplayShows.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
-import {useEffect} from "react"
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const DisplayShows = () => {
+  const [series, setSeries] = useState([])
 
   useEffect(() => {
-    fetchSeries()
+    const fetchData = async () => {
+      let response = await axios.get('https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga')
+      debugger
+      setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
+    }
 
+    fetchData();
   }, [])
 
-  const fetchSeries = () => {
-    debugger
-  }
-
   return (
-    <div>
-
-    </div>
+    <div>{series.map((serie) => {
+      return (
+        <div className="display-show" data-cy="display-show">
+          <img
+            src={serie.content.images.landscape.url}
+            alt={serie.id}
+            key={serie.id}
+          />
+        </div>
+      );
+    })}</div>
   )
 }
+
+
+
 
 export default DisplayShows
